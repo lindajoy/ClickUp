2a5ba842234aef43b410bd492c0e7e6a0296086b
lindajoy <wawirajoy98@gmail.com>
2022-07-14 06:37:50 +0300

    💡 Added Rick and morty interface

---
+++ src/app/shared/rick-morty-locations.ts
@@ -0,0 +1,10 @@
+
+export interface RickMortyLocations {
+    id: string,
+    name: string,
+    type: string,
+    dimension: string,
+    residents: string[],
+    url: string,
+    date: string
+}
